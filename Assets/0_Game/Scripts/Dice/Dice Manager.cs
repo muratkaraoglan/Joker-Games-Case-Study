5c4c474d3244af510d2e9f206c4b15fa9953c0df
muratkaraoglan <m.karaoglan1604@gmail.com>
2024-05-17 21:08:24 +0300

    Dice Throw

---
+++ Assets/0_Game/Scripts/Dice/Dice Manager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class DiceManager : MonoBehaviour
+{
+    [SerializeField] private Dice _dicePrefab;
+    [SerializeField] private int _diceInitCount = 2;// for test
+    [SerializeField] private float _throwForce = 3;
+    [SerializeField] private float _rollForce = 3;
+
+    private List<int> resultList = new List<int>();
+    [SerializeField] private List<GameObject> _throwedDice = new List<GameObject>();
+    // for test
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Roll();
+        }
+    }
+
+    void AddResult(int result)
+    {
+        resultList.Add(result);
+
+        if (resultList.Count == _diceInitCount)
+        {
+            print("Result: " + resultList.Sum());
+            resultList.Clear();
+        }
+    }
+
+    private async void Roll()
+    {
+        if (_dicePrefab == null) return;
+
+        _throwedDice.ForEach(dice => Destroy(dice));
+        _throwedDice.Clear();
+
+        for (int i = 0; i < _diceInitCount; i++)
+        {
+            Dice dice = Instantiate(_dicePrefab, transform.position, Quaternion.identity);
+            dice.Throw(_throwForce, _rollForce, (r) => { AddResult(r); });
+            _throwedDice.Add(dice.gameObject);
+
+            await Task.Yield();
+            await Task.Yield();
+            await Task.Yield();
+            await Task.Yield();
+            await Task.Yield();
+        }
+
+    }
+}
---
+++ Assets/0_Game/Scripts/Dice/Dice.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Dice : MonoBehaviour
+{
+    [SerializeField] private Transform[] _diceSides;
+    Action<int> _result;
+    private Rigidbody _rigidbody;
+    private bool _delayedCheck;
+    private bool _hasRotationStopped;
+
+    private void OnEnable()
+    {
+        _delayedCheck = true;
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    private void Update()
+    {
+        if (_delayedCheck) return;
+
+        if (!_hasRotationStopped && _rigidbody.velocity.sqrMagnitude <= .1f)
+        {
+            _hasRotationStopped = true;
+            GetRollResult();
+        }
+    }
+
+    [ContextMenu("Top Side")]
+    private void GetRollResult()
+    {
+        if (_diceSides.Length == 0) return;
+
+        var currentSideIndex = 0;
+        float currentY = _diceSides[0].position.y;
+
+        for (int i = 0; i < _diceSides.Length; i++)
+        {
+            if (currentY < _diceSides[i].position.y)
+            {
+                currentY = _diceSides[i].position.y;
+                currentSideIndex = i;
+            }
+        }
+        _result.Invoke(currentSideIndex + 1);
+        // print("Top Side " + (currentSideIndex + 1));
+    }
+
+    public void Throw(float throwForce, float rollForce, Action<int> result)
+    {
+        _result = result;
+        float randomValue = UnityEngine.Random.Range(-1f, 1f);
+        _rigidbody.AddForce(transform.forward * (throwForce + randomValue), ForceMode.Impulse);
+
+        float randomX = UnityEngine.Random.Range(0, 1f);
+        float randomY = UnityEngine.Random.Range(0, 1f);
+        float randomZ = UnityEngine.Random.Range(0, 1f);
+
+        //_rigidbody.AddTorque(, ForceMode.Impulse);
+        DelayedTorque(new Vector3(randomX, randomY, randomZ) * (rollForce + randomValue));
+        Delay();
+    }
+
+    private async void DelayedTorque(Vector3 torque)
+    {
+        await Task.Delay(100);
+        _rigidbody.AddTorque(torque, ForceMode.Impulse);
+    }
+
+    private async void Delay()
+    {
+
+        await Task.Delay(1000);
+        _delayedCheck = false;
+    }
+
+}
